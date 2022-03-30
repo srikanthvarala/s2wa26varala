905ba3a3c87134682a8ccac0e4616e7ef8c1eace
srikanth <s546826@nwmissouri.edu>
2022-03-30 13:03:37 -0500

    Added router for users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var times =0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  times =  times+1;
+  res.send('Access count is :'+times);
 });
 
 module.exports = router;
